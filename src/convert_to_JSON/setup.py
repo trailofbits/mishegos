133384d61f2e8a32481a165d044f31e00497319e
pothastm <tmppothast@gmail.com>
2020-01-23 13:42:28 -0500

    ran black for format

--- src/convert_to_JSON/setup.py
+++ src/convert_to_JSON/setup.py
@@ -10,9 +10,10 @@ class ComplexEncoder(json.JSONEncoder):
         # Let the base class default method raise the TypeError
         return json.JSONEncoder.default(self, obj)
 
-#intergrating into build crea
-#create script of getting proper file
-class FormattingJSON():
+
+# intergrating into build crea
+# create script of getting proper file
+class FormattingJSON:
     # opening file reading from
     output_file = Cohort.from_io(sys.stdin.buffer)
     # get total number of cohorts for the given file
@@ -26,13 +27,15 @@ class FormattingJSON():
             each_worker = {
                 "status": {
                     "value": output_file.total_cohorts[i].type[i_workers].status.value,
-                    "name": output_file.total_cohorts[i].type[i_workers].status.name.upper()
+                    "name": output_file.total_cohorts[i]
+                    .type[i_workers]
+                    .status.name.upper(),
                 },
                 "ndecoded": output_file.total_cohorts[i].type[i_workers].ndecoded,
                 "workerno": output_file.total_cohorts[i].type[i_workers].workerno,
                 "worker_so": output_file.total_cohorts[i].type[i_workers].worker_so,
                 "len": output_file.total_cohorts[i].type[i_workers].len,
-                "result": output_file.total_cohorts[i].type[i_workers].result
+                "result": output_file.total_cohorts[i].type[i_workers].result,
             }
             output_list.append(each_worker)
 
@@ -40,9 +43,8 @@ class FormattingJSON():
         cohorts = {
             "nworkers": num_workers,
             "inputs": output_file.total_cohorts[i].input,
-            "outputs": output_list
+            "outputs": output_list,
         }
         # convert each Json to file
         to_json = json.dumps(cohorts, cls=ComplexEncoder)
-        print (to_json, file=sys.stdout)
-
+        print(to_json, file=sys.stdout)
