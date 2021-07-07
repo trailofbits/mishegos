d1c7c930b8d4aa679a0f47c740b693a11ea5b976
iximeow <me@iximeow.net>
2021-07-07 07:43:29 -0700

    update yaxpeax worker to use 1.0 (#967)
    also uses a static mut to decode into to increase the chances of finding
    yaxpeax bugs from reusing old data

--- src/worker/yaxpeax-x86/src/lib.rs
+++ src/worker/yaxpeax-x86/src/lib.rs
@@ -13,22 +13,30 @@ use crate::mishegos::{decode_result, MISHEGOS_DEC_MAXLEN, decode_status_S_SUCCES
 #[no_mangle]
 pub static mut worker_name: *const c_char = b"yaxpeax-x86-mishegos\x00".as_ptr() as *const i8;
 
+static mut INSTR: Option<amd64::Instruction> = None;
 #[no_mangle]
 pub extern "C" fn try_decode(decode_result: *mut decode_result, bytes: *const u8, length: u8) {
+    unsafe {
+        if INSTR.is_none() {
+            INSTR = Some(amd64::Instruction::default());
+        }
+    }
     let decode_result = unsafe { decode_result.as_mut().expect("decode_result is not null") };
     let data = unsafe {
         std::slice::from_raw_parts(bytes.as_ref().expect("bytes is not null"), length as usize)
     };
     let decoder = amd64::InstDecoder::default();
+    let mut reader = yaxpeax_arch::U8Reader::new(data);
 
-    match decoder.decode(data.iter().cloned()) {
+    match decoder.decode_into(unsafe { INSTR.as_mut().unwrap() }, &mut reader) {
         Err(amd64::DecodeError::ExhaustedInput) => {
             decode_result.status = decode_status_S_PARTIAL;
         }
         Err(_error) => {
             decode_result.status = decode_status_S_FAILURE;
         }
-        Ok(instr) => {
+        Ok(()) => {
+            let instr = unsafe { INSTR.as_ref().unwrap() };
             decode_result.ndecoded = 0u64.wrapping_offset(instr.len()) as u16;
             let text = instr.to_string();
             assert!(text.len() < MISHEGOS_DEC_MAXLEN as usize);
